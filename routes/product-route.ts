a0b1da31a9dc501ecc5aab56b35235c4e9019dc4
Lautaro Decima <decimalautaro@gmail.com>
2022-03-27 21:00:58 -0300

    creacion de product route

---
+++ routes/product-route.ts
@@ -0,0 +1,26 @@
+import { request, response, Router } from "express";
+import ProductController from "../src/controllers/ProductController"
+
+
+const routerProduct = Router();
+
+const productController = new ProductController();
+
+
+routerProduct.get("/products", productController.list);
+
+routerProduct.get("/add", (request, response) => {
+  response.render("add");
+});
+
+routerProduct.post("/add-product", productController.create);
+
+routerProduct.get("/search-product", productController.search);
+
+routerProduct.get("/edit-product", productController.edit);
+
+routerProduct.post("/edit-product", productController.update);
+
+routerProduct.post("/delete-product", productController.delete);
+
+export { routerProduct };
