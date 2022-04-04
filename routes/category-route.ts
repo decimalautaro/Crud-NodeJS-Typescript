f6b7559171e0d0595da5d80f539eae8c9f89a982
Lautaro Decima <decimalautaro@gmail.com>
2022-04-04 12:39:50 -0300

    creacion de category-route

---
+++ routes/category-route.ts
@@ -0,0 +1,26 @@
+import { request, response, Router } from "express";
+import CategoryController from "../src/controllers/CategoryController"
+
+
+const routerCategory = Router();
+
+const categoryController = new CategoryController();
+
+
+routerCategory.get("/category", categoryController.list);
+
+routerCategory.get("/add-category", (request, response) => {
+  response.render("../views/category/category-add");
+});
+
+routerCategory.post("/add-category", categoryController.create);
+
+routerCategory.get("/search-category", categoryController.search);
+
+routerCategory.get("/edit-category", categoryController.edit);
+
+routerCategory.post("/edit-category", categoryController.update);
+
+routerCategory.post("/delete-category", categoryController.delete);
+
+export { routerCategory };
