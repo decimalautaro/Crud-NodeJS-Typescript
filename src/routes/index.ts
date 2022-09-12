c34ccaf577af72b3a8099b5efaa26920978e3446
Lautaro Decima <decimalautaro@gmail.com>
2022-09-12 15:50:34 -0300

    feat:agregando rutas a routerIndex

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { Request, Response } from 'express';
-import auth from '../lib/auth';
-
-const RouterIndex= express.Router();
 
 
-RouterIndex.get("/", auth.isLoggedIn, (request:Request, response:Response) => {
+const routerIndex= express.Router();
+
+
+routerIndex.get("/", (request:Request, response:Response) => {
     response.render("../views/index");
   });
+
+
+export {routerIndex};
