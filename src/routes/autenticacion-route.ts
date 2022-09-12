c9d77e5a41e19554d0501c39d72b8df50008184e
Lautaro Decima <decimalautaro@gmail.com>
2022-09-12 14:39:42 -0300

    fix: modificando routes de autenticacion

--- src/routes/autenticacion-route.ts
+++ src/routes/autenticacion-route.ts
@@ -2,16 +2,17 @@ import express from 'express';
 import { Request, Response } from 'express';
 import { Router } from 'express';
 import passport from 'passport'
+import auth from '../lib/auth'
 
 const router = Router();
 
 // SIGNUP
 
-router.get('/signup', (req: Request, res: Response) => {
+router.get('/signup', auth.isNotLoggedIn, (req: Request, res: Response) => {
     res.render('auth/signup')
 })
 
-router.post('/signup', passport.authenticate('local.signup', {
+router.post('/signup', auth.isNotLoggedIn, auth.isNotLoggedIn, passport.authenticate('local.signup', {
     successRedirect: '/signin',
     failureRedirect: '/signup',
     failureFlash: true
@@ -20,20 +21,25 @@ router.post('/signup', passport.authenticate('local.signup', {
 
 // SIGNIN
 
-router.get('/signin', (req: Request, res: Response) => {
+router.get('/signin', auth.isNotLoggedIn, (req: Request, res: Response) => {
     res.render('auth/signin')
 })
 
-router.post('/signin', (req: Request, res: Response, next) => {
+router.post('/signin', auth.isNotLoggedIn, (req: Request, res: Response, next) => {
     passport.authenticate('local.signin', {
-        successRedirect: '/',
+        successRedirect: '/profile',
         failureRedirect: '/signin',
         failureFlash: true
     })(req, res, next);
+    
 
 });
 
-router.get('/logout', function(req: Request, res:Response, next) {
+router.get("/profile", auth.isLoggedIn, (request, response) => {
+    response.render("../views/profile");
+  });
+
+router.get('/logout', auth.isLoggedIn, function(req: Request, res:Response, next) {
     req.logout(function(err) {
       if (err) { return next(err); }
       req.flash('message', 'Gracias por usar la app');
