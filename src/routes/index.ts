4b7c836707d87e282faff9e498cbd41f3d12e89e
Lautaro Decima <decimalautaro@gmail.com>
2022-09-18 02:04:48 -0300

    creando estilos de login

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { Request, Response } from 'express';
+import auth from '../lib/auth';
+
 
 
 const routerIndex= express.Router();
 
 
-routerIndex.get("/", (request:Request, response:Response) => {
+routerIndex.get("/",  (request:Request, response:Response) => {
     response.render("../views/index");
   });
 
